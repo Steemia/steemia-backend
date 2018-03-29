ada5240982c04f51eaead953d037708dec5d7a2c
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-03-29 01:55:49 -0400

    comments endpoint

--- routes/comments_votes.js
+++ routes/comments_votes.js
@@ -1,39 +1,45 @@
 var exports = module.exports = {};
 var Comments = require('../models/comments');
-var Posts = require('../models/posts');
 var Helper = require('./helper');
 var Util = require('../utils/utils');
 var steem = require('steem');
-var request = require('request');
-var Promise = require("bluebird");
 
 function get_comments(req, res) {
-    let author = 'jaysermendez';
-    let permlink = 'steemia/@jaysermendez/steemia-full-text-search-text';
+    let username = req.query.username;
+    let permlink = decodeURIComponent(req.query.permlink);
+    let to_delete = permlink.split('/')[2] + "/" + permlink.split('/')[3];
+    to_delete = to_delete.replace("@", '');
+    
     let comments = [];
     steem.api.getState(permlink, (err, results) => {
         results = JSON.parse(JSON.stringify(results));
-        delete results.content['jaysermendez/steemia-full-text-search-text'];
+        delete results.content[to_delete];
 
         var result = Object.values(results.content)
         result.sort(function(a, b) { 
             return b.id > a.id;   // <== to compare string values
         });
-        res.send(result)
-    })
-    // steem.api.getContentReplies(author, permlink, (err, replies) => {
 
-    //     let result = replies.map(reply => {
-    //         comments.push(reply)
-    //         if (reply.children > 0) {
-
-    //             return 
-    //         }
-
-    //     })
-
-    //     res.send(post.content)
-    // })
+        var final = result.map(comment => {
+            return {
+                body: comment.body,
+                avatar: "https://img.busy.org/@" + comment.author,
+                created: comment.created,
+                url: comment.url,
+                author_reputation: Util.reputation(comment.author_reputation),
+                author: comment.author,
+                category: comment.category,
+                net_votes: comment.net_votes,
+                net_likes: comment.net_likes,
+                pending_payout_value: parseFloat(comment.total_payout_value) + parseFloat(comment.pending_payout_value),
+                vote: Helper.is_post_voted(username, comment),
+                children: comment.children
+            }
+        });
+        res.send({
+            results: final
+        })
+    });
 }
 
 
