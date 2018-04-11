dd8412315772d06add942e294496c885056696da
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-04-11 00:11:41 -0400

    fix regex for outside image to catch more

--- routes/helper.js
+++ routes/helper.js
@@ -94,7 +94,7 @@ function get_body_image(post) {
     }
 
     catch (e) {
-        let n = post.body.match(/([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/i)
+        let n = post.body.match(/https?:\/\/(?:[-a-zA-Z0-9._]*[-a-zA-Z0-9])(?::\d{2,5})?(?:[/?#](?:[^\s"'<>\][()]*[^\s"'<>\][().,])?(?:(?:\.(?:tiff?|jpe?g|gif|png|svg|ico)|ipfs\/[a-z\d]{40,})))/gi)
         if (n === null) image = null;
         else {
             image = n[0];
